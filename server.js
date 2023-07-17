cdadaf965c7dc227b444f8743f493ed4af8ca9f6
Aubrey Moya-Mendez Trotter <Aubrey@adminisatorsmbp.mynetworksettings.com>
2023-07-17 16:54:31 -0400

    Updates before test deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 5000;
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with the origin of your frontend server
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
