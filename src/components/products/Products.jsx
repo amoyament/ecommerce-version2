7a247caa603c15f9fdcac7706054325f197d6c0b
Aubrey Moya-Mendez Trotter <Aubrey@adminisatorsmbp.mynetworksettings.com>
2023-07-16 23:50:13 -0400

    All components rendered and functional. Still need to fix header/footer spacing and cleanup code.

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,23 +1,3 @@
-// //This will be your homepage!!
-// //type rafce and it will build basic structre of your component
-// import React from "react";
-// import StyleSection from "./ModelStyle.jsx";
-// // import ProductComponent from "./TestProduct.jsx";
-// import ProductList from "./ProductList.jsx";
-
-// const Products = () => {
-//   return (
-//     <div>
-//       <StyleSection />
-//       {/* <RenderProducts /> */}
-//       {/* <ProductComponent /> */}
-//       <ProductList />
-//     </div>
-//   );
-// };
-
-// export default Products;
-
 import React, { useState, useEffect } from "react";
 import StyleSection from "./ModelStyle.jsx";
 import ProductList from "./ProductList.jsx";
